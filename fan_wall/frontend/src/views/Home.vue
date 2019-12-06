6b0c9be8511859c7ba100a9ff264920e14f9c6b5
Ute Bracklow <ute.bracklow@moccu.com>
2019-12-06 17:57:30 +0100

    Adds content to home

--- fan_wall/frontend/src/views/Home.vue
+++ fan_wall/frontend/src/views/Home.vue
@@ -11,4 +11,19 @@
     padding: 40px 0;
     min-height: 100vh;
   }
+</style><template lang="pug">
+  main
+      div#intro
+        h1 ⭐️ ⭐️ ⭐️ WELCOME TO FANWALL ⭐️ ⭐️ ⭐️ 
+    
+
+      <iframe src="https://giphy.com/embed/aSNEriP43LGus" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+
+</template>
+
+<style scoped>
+  #intro {
+    padding: 10px 0;
+    color: white;
+  }
 </style>

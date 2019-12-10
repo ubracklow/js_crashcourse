c1fab335b3701752ec93bbbdba0c1270b97454a2
Ute Bracklow <ute.bracklow@moccu.com>
2019-12-10 13:18:47 +0100

    Adds component for other attendees

---
+++ fan_wall/frontend/src/components/attendee-element.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'AttendeeElement',
+  props: ['attendee']
+}
+</script>
+
+<template lang="pug">
+  article.card 
+    h2 {{ attendee.name }} from {{ attendee.hometown }} ⭐️
+</template>
+
+<style scoped>
+.card {
+  color: #b12183;
+}
+</style>
